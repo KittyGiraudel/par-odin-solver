6f3a8ca80902c9980c530de5f96f6c97040763f4
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-02-02 10:57:05 +0100

    Add a workflow to deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: 'src/web',
+  base: '/par-odin-solver/',
   plugins: [react()],
   build: {
     outDir: '../../dist-web',
